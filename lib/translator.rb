f3b698061983e4e4559af58bf40390a3396a1587
Dan Foley <dan@dan-foley.com>
2019-12-19 17:58:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,19 +9,23 @@ def load_library(path)
   lib = YAML.load_file(path)
   new_hash = {}
   lib.each do |meaning, eng_jp|
+    
       # checks for the key existence
       if !new_hash[:get_meaning]
         # if doesn't exist make it an empty hash
         new_hash[:get_meaning] = {}
       end
+      
       # checks for the key/value pair existence
       if !new_hash[:get_meaning][eng_jp[1]]
         # if doesn't exist create the appropriate k/v pair
         new_hash[:get_meaning][eng_jp[1]] = meaning
       end
+      
       if !new_hash[:get_emoticon]
         new_hash[:get_emoticon] = {}
       end
+      
       if !new_hash[:get_emoticon][eng_jp[0]]
         new_hash[:get_emoticon][eng_jp[0]] = eng_jp[1]
       end
